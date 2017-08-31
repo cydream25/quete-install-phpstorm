005cd5cbe0bd29c2c6eb5da1b42699773e6eea97
Sylvain HECK <sylvain.heck@wildcodeschool.fr>
2017-08-31 18:14:10 +0200

    Initial commit

---
+++ age.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 31/08/17
+ * Time: 16:11
+ */
+$students = [
+    "Emmanuel"  => 42,
+    "Thierry"   => 51,
+    "Pascal"    => 45,
+    "Eric"      => 48,
+    "Nicolas"   => 19,
+];
+
+foreach ($students as $name=>$age) {
+    echo "$name a $age ans\n";
+    $somme +=$age;
+}
+
+echo "moyenne d'age : ".$somme/count($students)."\n";
